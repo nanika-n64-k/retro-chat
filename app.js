72d54d25fc64b3564bbc0081238d70137322f42c
nanika-n64-k <nanika.n64@gmail.com>
2026-05-12 17:52:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>YukiBBS</title>
 
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div id="header">
+  YukiBBS
+</div>
+
+<div id="messages"></div>
+
+<div id="input-area">
+  <input id="name" placeholder="name">
+
+  <input id="seed" placeholder="seed">
+
+  <input id="message" placeholder="message">
+
+  <button id="send">
+    send
+  </button>
+</div>
+
+<script src="app.js"></script>
+
+</body>
+</html>
